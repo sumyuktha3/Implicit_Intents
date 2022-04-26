f3f82737e1ad9d6ad1daf51b2520b913105304e1
sumyuktha3 <75235818+sumyuktha3@users.noreply.github.com>
2022-04-26 20:56:07 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/exno2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.exno2;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(view -> {
